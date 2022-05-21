aca789bad2d4beec7f19360e6e61cc36427b0be9
Jacob Kofron <gnomical@users.noreply.github.com>
2022-05-20 23:38:30 -0400

    prettier

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,25 +1,25 @@
-import { 
-  createEffect,
-  createSignal,
-} from 'solid-js';
-import fallbackStyles from './fallbacks.module.scss';
-import fallbackThemes from './fallbacks.themes.json';
+import { createEffect, createSignal } from "solid-js";
+import fallbackStyles from "./fallbacks.module.scss";
+import fallbackThemes from "./fallbacks.themes.json";
 
-type ThemeProviderProps = { 
+type ThemeProviderProps = {
   label: string;
-  styles: any; 
+  styles: any;
   themes: any;
 };
 
 export function ThemeProvider(props: ThemeProviderProps) {
-
   const themes = props.themes || fallbackThemes;
   const styles = props.styles || fallbackStyles;
 
   const systemThemeIsDark = window.matchMedia("(prefers-color-scheme: dark)");
-  const [currentTheme, setTheme] = createSignal(systemThemeIsDark ? themes.system_themes.dark : themes.system_themes.light);
-  const [otherTheme, setOtherTheme] = createSignal(systemThemeIsDark ? themes.system_themes.light : themes.system_themes.dark);
-// 
+  const [currentTheme, setTheme] = createSignal(
+    systemThemeIsDark ? themes.system_themes.dark : themes.system_themes.light
+  );
+  const [otherTheme, setOtherTheme] = createSignal(
+    systemThemeIsDark ? themes.system_themes.light : themes.system_themes.dark
+  );
+
   systemThemeIsDark.addEventListener("change", e => {
     if (e.matches) {
       toggleTheme(themes.system_themes.dark);
@@ -30,7 +30,10 @@ export function ThemeProvider(props: ThemeProviderProps) {
 
   createEffect(() => {
     Object.keys(themes[currentTheme()].colors).forEach(name => {
-      document.documentElement.style.setProperty("--stp-" + name, themes[currentTheme()].colors[name]);
+      document.documentElement.style.setProperty(
+        "--stp-" + name,
+        themes[currentTheme()].colors[name]
+      );
     });
   });
 
