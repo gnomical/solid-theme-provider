45a14e06742281625968acd9d31c068e7687c80c
Jacob Kofron <gnomical@users.noreply.github.com>
2022-12-15 23:11:46 -0500

    gitignore was blocking necessary files

---
+++ src/lib/types.ts
@@ -0,0 +1,38 @@
+
+export type ThemeConfig = {
+  icon: string;
+  browser_theme_color: string;
+};
+
+export type ThemeVars = {
+  [key: string]: string;
+};
+
+export type ThemeObject = {
+  config: ThemeConfig;
+  vars: ThemeVars;
+};
+
+export type SystemThemesObject = {
+  dark: string;
+  light: string;
+};
+
+export type ThemesObject = {
+  [key: string]: ThemeObject;
+};
+
+export type ThemesConfigObject = {
+  system_theme_config: SystemThemesObject;
+  themes: ThemesObject;
+};
+
+export type ThemeProviderProps = {
+  default?: string;
+  id?: string;
+  label?: string;
+  prefix?: string;
+  styles?: any;
+  themes?: ThemesConfigObject;
+  menu_placement: "ne" | "se" | "sw" | "nw";
+};
