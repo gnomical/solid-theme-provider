22504fc5bf20717a804e1931e168b239918ad58f
Jacob Kofron <gnomical@users.noreply.github.com>
2024-11-25 16:12:16 -0500

    updated packages, modified build output

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,7 @@
-import babel from "@rollup/plugin-babel";
 import json from "@rollup/plugin-json";
-import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import jsx from "acorn-jsx";
-
-const plugins = [
-  nodeResolve({
-    extensions: [".js", ".ts", ".tsx"],
-  }),
-  babel({
-    extensions: [".js", ".ts", ".tsx"],
-    babelHelpers: "bundled",
-    presets: ["@babel/preset-typescript"],
-    exclude: "node_modules/**",
-  }),
-];
 
 export default {
   input: "src/index.tsx",
@@ -27,7 +13,11 @@ export default {
   ],
   external: ["solid-js"],
   plugins: [
-    typescript({ compilerOptions: { jsx: "preserve" } }),
+    typescript({ 
+      compilerOptions: { 
+        jsx: "preserve",
+      } 
+    }),
     json(),
     postcss({
       minimize: true,
