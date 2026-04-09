34d61af55aadc634648d1f94c45e6804b995a278
Jacob Kofron <gnomical@users.noreply.github.com>
2026-04-08 20:58:00 -0400

    chore: demo starter

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import json from "@rollup/plugin-json";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
-import jsx from "acorn-jsx";
 
 export default {
   input: "src/index.tsx",
@@ -26,5 +25,4 @@ export default {
       plugins: [],
     }),
   ],
-  acornInjectPlugins: [jsx()],
 };
