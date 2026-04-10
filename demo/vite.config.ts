ac03d544ca3817f963ef958d105ff36c5c9274a9
Jacob Kofron <gnomical@users.noreply.github.com>
2026-04-09 22:39:45 -0400

    fix: build

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite"
 import { nitroV2Plugin as nitro } from "@solidjs/vite-plugin-nitro-2"
-
 import { solidStart } from "@solidjs/start/config"
+import { resolve } from "path"
+import { fileURLToPath } from "url"
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url))
 
 export default defineConfig({
   plugins: [solidStart(), nitro()],
+  resolve: {
+    alias: {
+      "solid-theme-provider": resolve(__dirname, "../src/index.tsx"),
+    },
+  },
 })
