a59d0764d6e34e78db064d63a9771b912e444dbc
Jacob Kofron <gnomical@users.noreply.github.com>
2026-04-10 21:21:45 -0400

    fix: assistive semantics

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -5,14 +5,14 @@ type IconButtonProps = {
   icon: JSX.Element
   label?: string
   classList?: Record<string, boolean>
-  onMouseDown?: JSX.EventHandlerUnion<HTMLDivElement, MouseEvent>
+  onMouseDown?: JSX.EventHandlerUnion<HTMLButtonElement, MouseEvent>
 }
 
 export function IconButton(props: IconButtonProps) {
   return (
-    <div class={styles.button} classList={props.classList} onMouseDown={props.onMouseDown}>
+    <button type="button" class={styles.button} classList={props.classList} onMouseDown={props.onMouseDown}>
       <span class={styles.icon}>{props.icon}</span>
       {props.label && <span>{props.label}</span>}
-    </div>
+    </button>
   )
 }
